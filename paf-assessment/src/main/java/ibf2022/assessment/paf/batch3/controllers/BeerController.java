8d9aa3ab12aa902a71438d40ec052b64cd6d6695
Charleswcl <wang90cl@hotmail.com>
2023-04-28 11:45:23 +0800

    Task1

--- paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/controllers/BeerController.java
+++ paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/controllers/BeerController.java
@@ -1,16 +1,44 @@
 package ibf2022.assessment.paf.batch3.controllers;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ibf2022.assessment.paf.batch3.models.Style;
+import ibf2022.assessment.paf.batch3.services.BeerService;
+
+@Controller
+@RequestMapping("/api/beer")
+
 public class BeerController {
 
-	//TODO Task 2 - view 0
-	
-	
-	//TODO Task 3 - view 1
-	
+	@Autowired
+	private BeerService beerSvc;
+	// TODO Task 2 - view 0
 
-	//TODO Task 4 - view 2
+	@GetMapping(path ="/styles", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String getStylesWithCount (@RequestBody String form, Model model) {
 
-	
-	//TODO Task 5 - view 2, place order
+		List<Style> styles = beerSvc.getAllStyleCount();
+
+		model.addAttribute("styles", styles);
+		 return "view0";
+
+	}
+
+
+	// TODO Task 3 - view 1
+
+	// TODO Task 4 - view 2
+
+	// TODO Task 5 - view 2, place order
 
 }
--- paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/BeerRepository.java
+++ paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/BeerRepository.java
@@ -1,21 +1,53 @@
 package ibf2022.assessment.paf.batch3.repositories;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
 
 import ibf2022.assessment.paf.batch3.models.Beer;
 import ibf2022.assessment.paf.batch3.models.Brewery;
 import ibf2022.assessment.paf.batch3.models.Style;
 
+@Repository
 public class BeerRepository {
+
+	@Autowired
+	private JdbcTemplate template;
+
+	private final String COUNT_STYLES = """
+			select s.style_name as Style, count(style_id) as Beer_Count
+			from styles as s
+			join beers as b
+			on s.id = b.style_id
+			group by style_id
+			order by count(style_id) desc, style_name asc;
+
+							""";
 
 	// DO NOT CHANGE THE SIGNATURE OF THIS METHOD
 	public List<Style> getStyles() {
 		// TODO: Task 2
 
-		return null;
+		final List<Style> styleList = new LinkedList<>();
+
+		final SqlRowSet rs = template.queryForRowSet(COUNT_STYLES);
+
+		while (rs.next()){
+			Style style = new Style();
+			style.setStyleId(rs.getInt("id"));
+			style.setName(rs.getString("Style"));
+			style.setBeerCount(rs.getInt("Beer_Count"));
+			styleList.add(style);
+		}
+
+		return styleList;
 	}
-		
+
 	// DO NOT CHANGE THE METHOD'S NAME OR THE RETURN TYPE OF THIS METHOD
 	public List<Beer> getBreweriesByBeer(/* You can add any number parameters here */) {
 		// TODO: Task 3
